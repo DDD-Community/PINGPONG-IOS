507cc0303b77da7bb1d5c84b4b0d307a9ad11cf4
Roy-wonji <shuwj81@daum.net>
2023-06-25 21:04:41 +0900

    ✨[feat]:  demoApp에  onBoardingApp 구현

---
+++ PingPong/Projects/DemoApp/OnBoardingApp/Sources/Application/SearchApp.swift
@@ -0,0 +1,16 @@
+//
+//  SearchApp.swift
+//  PingPongProject
+//
+//  Created by Byeon jinha on 2023/06/01.
+//
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SearchMainView()
+        }
+    }
+}
