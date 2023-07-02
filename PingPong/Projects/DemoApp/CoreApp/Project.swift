7aeb5d2c58de3f11a793229c44dbce0e5beb28ea
Roy-wonji <shuwj81@daum.net>
2023-07-02 21:08:01 +0900

    ✨[feat]:  demoApp 에  core app 생성

--- PingPong/Projects/DemoApp/CoreApp/Project.swift
+++ PingPong/Projects/DemoApp/CoreApp/Project.swift
@@ -1,8 +1,36 @@
 //
-//  Project.swift
+//  CoreApp.swift
 //  Manifests
 //
 //  Created by 서원지 on 2023/07/02.
 //
 
-import Foundation
+import ProjectDescription
+import MyPlugin
+
+
+
+let localHelper = LocalHelper(name: "MyPlugin")
+let project = Project.makeModule(
+    name: "CoreApp",
+    bundleId: .appBundleID(name: "CoreApp"),
+    platform: .iOS,
+    product: .app,
+    //MARK: - 풀 빌드 할때는 프레임 워크로 변경
+//    product: .staticFramework,
+    packages: [ // packages를 추가하여 Amplify 라이브러리 추가
+              ],
+    
+    
+    setting: .appMainSetting,
+    dependencies: [
+        .Projcet.Core
+            
+    ],
+    sources: ["Sources/**", "Resources/**"],
+    resources: ["Resources/**", "Sources/**"],
+    infoPlist: .file(path: "Support/CoreAppInfo.plist"),
+    entitlements: .relativeToCurrentFile("Entitlements/CoreApp.entitlements")
+//    scheme: [realseScheme, debugScheme]
+)
+
---
+++ PingPong/Projects/DemoApp/CoreApp/Sources/Application/CoreApp.swift
@@ -0,0 +1,16 @@
+//
+//  CoreApp.swift
+//  PingPongProject
+//
+//  Created by Byeon jinha on 2023/06/01.
+//
+import SwiftUI
+
+@main
+struct CoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
