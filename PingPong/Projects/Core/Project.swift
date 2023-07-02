38842febcfcd200d256f6d903d64d26fa77edbd8
Roy-wonji <shuwj81@daum.net>
2023-07-02 21:08:57 +0900

    ✨[feat]:  core app 생성

--- PingPong/Projects/Core/Project.swift
+++ PingPong/Projects/Core/Project.swift
@@ -5,4 +5,27 @@
 //  Created by 서원지 on 2023/07/02.
 //
 
-import Foundation
+
+import ProjectDescription
+import MyPlugin
+
+
+
+let localHelper = LocalHelper(name: "MyPlugin")
+let project = Project.makeModule(
+    name: "Core",
+    bundleId: .appBundleID(name: "Core"),
+    product: .staticFramework,
+    setting:  .appBaseSetting,
+    dependencies: [
+        .SPM.Inject,
+        .Projcet.OnBoarding,
+        .Projcet.Home,
+        .Projcet.Profile,
+        .Projcet.Authentication,
+        .Projcet.Search
+        
+    ],
+    sources: ["Sources/**"]
+//    resources: ["Resources/**"]
+)
