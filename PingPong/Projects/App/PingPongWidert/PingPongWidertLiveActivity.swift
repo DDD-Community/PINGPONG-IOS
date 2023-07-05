0c4a96328ddbba6247dce98202fdeee8b3781df3
Roy-wonji <shuwj81@daum.net>
2023-07-05 22:16:35 +0900

    ✨[feat]:  앱 위젟 셋업

---
+++ PingPong/Projects/App/PingPongWidert/PingPongWidertLiveActivity.swift
@@ -0,0 +1,78 @@
+//
+//  PingPongWidertLiveActivity.swift
+//  PingPongWidert
+//
+//  Created by 서원지 on 2023/07/02.
+//  Copyright © 2023 Wonji Suh. All rights reserved.
+//
+
+import ActivityKit
+import WidgetKit
+import SwiftUI
+
+struct PingPongWidertAttributes: ActivityAttributes {
+    public struct ContentState: Codable, Hashable {
+        // Dynamic stateful properties about your activity go here!
+        var value: Int
+    }
+
+    // Fixed non-changing properties about your activity go here!
+    var name: String
+}
+
+struct PingPongWidertLiveActivity: Widget {
+    var body: some WidgetConfiguration {
+        ActivityConfiguration(for: PingPongWidertAttributes.self) { context in
+            // Lock screen/banner UI goes here
+            VStack {
+                Text("Hello")
+            }
+            .activityBackgroundTint(Color.cyan)
+            .activitySystemActionForegroundColor(Color.black)
+
+        } dynamicIsland: { context in
+            DynamicIsland {
+                // Expanded UI goes here.  Compose the expanded UI through
+                // various regions, like leading/trailing/center/bottom
+                DynamicIslandExpandedRegion(.leading) {
+                    Text("Leading")
+                }
+                DynamicIslandExpandedRegion(.trailing) {
+                    Text("Trailing")
+                }
+                DynamicIslandExpandedRegion(.bottom) {
+                    Text("Bottom")
+                    // more content
+                }
+            } compactLeading: {
+                Text("L")
+            } compactTrailing: {
+                Text("T")
+            } minimal: {
+                Text("Min")
+            }
+            .widgetURL(URL(string: "http://www.apple.com"))
+            .keylineTint(Color.red)
+        }
+    }
+}
+
+struct PingPongWidertLiveActivity_Previews: PreviewProvider {
+    static let attributes = PingPongWidertAttributes(name: "Me")
+    static let contentState = PingPongWidertAttributes.ContentState(value: 3)
+
+    static var previews: some View {
+        attributes
+            .previewContext(contentState, viewKind: .dynamicIsland(.compact))
+            .previewDisplayName("Island Compact")
+        attributes
+            .previewContext(contentState, viewKind: .dynamicIsland(.expanded))
+            .previewDisplayName("Island Expanded")
+        attributes
+            .previewContext(contentState, viewKind: .dynamicIsland(.minimal))
+            .previewDisplayName("Minimal")
+        attributes
+            .previewContext(contentState, viewKind: .content)
+            .previewDisplayName("Notification")
+    }
+}
