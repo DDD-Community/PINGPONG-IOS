46bb1c8af0a12e2df27adfdf9e94a0d2b2a9b883
Roy-wonji <shuwj81@daum.net>
2023-08-26 16:48:11 +0900

    Revert "Merge branch 'feature/OnBoarding' of https://github.com/DDD-Community/PINGPONG-IOS into Dev"
    This reverts commit 3d6f78d656b6c9c372419bfad713e582f070966d, reversing
    changes made to 83f46d841d798bd92dc51b36a1d3ae95845d1ad2.

--- PingPong/Projects/Feature/OnBoarding/Sources/UI/View/MainView/OnBoardingView.swift
+++ PingPong/Projects/Feature/OnBoarding/Sources/UI/View/MainView/OnBoardingView.swift
@@ -8,33 +8,35 @@
 import SwiftUI
 import Inject
 import DesignSystem
-import Authorization
+
 
 public struct OnBoardingView: View {
+    
     @State private var serviceUseAgmentView: Bool = false
-    @ObservedObject private var i0 = Inject.observer
     
     public init() {
         //        self.i0 = i0
     }
+    
     public var body: some View {
         NavigationStack {
             ScrollView {
                 VStack {
                     Spacer()
-                        .frame(height: UIScreen.screenHeight / 8)
+                        .frame(height: UIScreen.screenHeight/8)
+                    
+                    Text("명언제과점에 오신걸 환영합니다")
+                        .bold()
+                        .font(.system(size: 22))
+                    
                     
                     loadingAnimationView()
-                    Text("명언제과점")
-                        .bold()
-                        .font(.custom("DNF Bit Bit TTF", size: 44))
-                        .foregroundColor(.orange)
                     
                     cookeWiseSayingView()
-                    //                    loginWithGoogle()
                     
                     socialLoginButtonView()
                     
+                    lookAroundService()
                     
                     Spacer(minLength: .zero)
                 }
@@ -43,6 +45,10 @@ public struct OnBoardingView: View {
             
             .navigationDestination(isPresented: $serviceUseAgmentView) {
                 ServiceUseAgmentView()
+            }
+            
+            .task {
+            
             }
         }
         
@@ -54,8 +60,9 @@ public struct OnBoardingView: View {
         Spacer()
             .frame(height: UIScreen.screenWidth/7)
         
-        Image(asset: .pingpongLogoOrange)
-            .padding(.bottom, 16)
+        Circle()
+            .fill(.gray.opacity(0.4))
+            .frame(width: 214, height: 214)
     }
     
     
@@ -77,75 +84,47 @@ public struct OnBoardingView: View {
             .frame(height: UIScreen.screenWidth/7)
         
         VStack(spacing: .zero)  {
-            //            GoogleSignInButton(action: handleSignInButton)
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.gray.opacity(0.7))
+                .frame(width: UIScreen.screenWidth - 80, height: UIScreen.screenHeight*0.06)
+                .overlay {
+                    Text("카카오톡으로 로그인")
+                        .font(.system(size: 18))
+                }
+            
             Spacer()
                 .frame(height: 12)
             
-            loginWithApple()
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.gray.opacity(0.7))
+                .frame(width: UIScreen.screenWidth - 80, height: UIScreen.screenHeight*0.06)
+                .overlay {
+                    Text("애플로 로그인")
+                        .font(.system(size: 18))
+                }
+            
+            
             
         }
     }
     
-    //    @ViewBuilder
-    //        private func loginWithGoogle() -> some View {
-    //            Spacer()
-    //                .frame(height: 20)
-    //
-    //            Button{
-    ////                viewModel.googleLogin()
-    //            } label: {
-    //                HStack(spacing: 10) {
-    //
-    //                    Spacer()
-    //
-    //                    Image(asset: .googleLogo)
-    //                        .resizable()
-    //                        .frame(width: 20, height: 20)
-    //                        .foregroundColor(Color.black)
-    //
-    //                    Text("구글 계정으로 계속하기")
-    //
-    //                    Spacer()
-    //                }
-    //            }
-    //
-    //            .frame(height: 50)
-    //            .cornerRadius(10)
-    //            .overlay(
-    //                RoundedRectangle(cornerRadius: 10)
-    //                    .stroke(Color.black, lineWidth: 1)
-    //
-    //            )
-    //            .padding(.horizontal, 40)
-    //        }
     @ViewBuilder
-    private func loginWithApple() -> some View {
+    private func lookAroundService() -> some View {
         Spacer()
-            .frame(height: 20)
-        RoundedRectangle(cornerRadius: 10)
-            .frame(height: 50)
-            .foregroundColor(Color.black)
-            .overlay(
-                ZStack {
-                    HStack(spacing: 10) {
-                        Image(asset: .appleLogo)
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                            .padding()
-                        
-                        Spacer()
-                    }
-                    
-                    Text("Apple로 계속하기")
-                        .foregroundColor(.white)
-                        .bold()
+            .frame(height: 22)
+        VStack(alignment: .center) {
+            Text("서비스 둘러보기")
+                .font(.system(size: 18))
+                .onTapGesture {
+                    serviceUseAgmentView.toggle()
                 }
-            )
-            .padding(.horizontal, 30)
-            .onTapGesture {
-                
-            }
+        }
     }
+    
 }
 
-
+public struct OnBoardingView_Previews: PreviewProvider {
+    public static var previews: some View {
+        OnBoardingView()
+    }
+}
