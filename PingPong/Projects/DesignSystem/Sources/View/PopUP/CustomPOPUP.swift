7adf35f62bd900a199c6f3b7b5766a4dfb5f4e75
Roy-wonji <shuwj81@daum.net>
2023-09-02 19:15:49 +0900

    ✨[feat]: 폰트  및  커스텀  팝업 구현

--- PingPong/Projects/DesignSystem/Sources/View/PopUP/CustomPOPUP.swift
+++ PingPong/Projects/DesignSystem/Sources/View/PopUP/CustomPOPUP.swift
@@ -16,23 +16,29 @@ public struct CustomPOPUP: View {
     var subTitle: String
     var useGif: Bool
     
-    public init(image: ImageAsset, title: String, subTitle: String, useGif: Bool) {
+    var confirmAction: () -> Void
+    
+    public init(image: ImageAsset, title: String, subTitle: String, useGif: Bool, confirmAction: @escaping () -> Void) {
         self.image = image
         self.title = title
         self.subTitle = subTitle
         self.useGif = useGif
+        self.confirmAction = confirmAction
     }
     
     public var body: some View {
         VStack {
             RoundedRectangle(cornerRadius: 16)
                 .fill(Color.basicWhite)
-                .frame(width: UIScreen.screenWidth - 40, height: 280)
+                .frame(width: UIScreen.screenWidth - 40, height: useGif ? 320: 300)
                 .overlay {
                     VStack {
                         popupImage()
                         
                         popupTitle()
+                        
+                        
+                        popupButton()
                     }
                 }
             
@@ -43,11 +49,17 @@ public struct CustomPOPUP: View {
     private func popupImage() -> some View {
         VStack {
             if useGif {
+                Spacer()
+                    .frame(height: 16)
+                
                 AnimatedImage(name: "swipe.gif", isAnimating: .constant(true))
                     .resizable()
                     .scaledToFit()
                     .frame(width: 141, height: 141)
             } else {
+                Spacer()
+                    .frame(height: 16)
+                
                 Image(asset: image)
                     .resizable()
                     .scaledToFit()
@@ -62,12 +74,61 @@ public struct CustomPOPUP: View {
             .frame(height: 8)
         
         VStack(spacing: 10) {
-            Text(title)
-                .pretendardFont(family: .SemiBold, size: 14)
-                .foregroundColor(.black)
-            
-            Text(subTitle)
-                .pretendardFont(family: .Bold, size: 16)
+            if useGif {
+                Text(title)
+                    .pretendardFont(family: .Bold, size: 18)
+                    .foregroundColor(.black)
+            } else {
+                Text(title)
+                    .pretendardFont(family: .SemiBold, size: 14)
+                    .foregroundColor(.black)
+                
+                Text(subTitle)
+                    .pretendardFont(family: .SemiBold, size: 14)
+                    .foregroundColor(.basicGray6)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func popupButton() -> some View {
+        Spacer()
+            .frame(height: 28)
+        
+        VStack {
+            if useGif {
+                HStack {
+//                    Spacer()
+                    
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(Color.primaryOrange)
+                        .frame(width: 140, height: 38)
+                        .overlay {
+                            Text("네 알겠습니다")
+                                .pretendardFont(family: .SemiBold, size: 18)
+                        }
+                        .onTapGesture {
+                            confirmAction()
+                        }
+                    
+                    
+//                    Spacer()
+                }
+            } else {
+                HStack {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(Color.primaryOrange)
+                        .frame(width: UIScreen.screenWidth/3, height: 48)
+                        .overlay {
+                            Text("확인")
+                                .pretendardFont(family: .SemiBold, size: 16)
+                        }
+                        .onTapGesture {
+                            confirmAction()
+                        }
+                    
+                }
+            }
         }
     }
 }
