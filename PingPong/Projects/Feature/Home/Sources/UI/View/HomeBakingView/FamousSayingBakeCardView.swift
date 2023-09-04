74e4db0adfbe7eea91a894b85a63e1cc53fdb803
lainyzine <jinhaday@gmai.com>
2023-09-05 00:24:45 +0900

    ✨[feat]: home_baking UI, Loigic #22

---
+++ PingPong/Projects/Feature/Home/Sources/UI/View/HomeBakingView/FamousSayingBakeCardView.swift
@@ -0,0 +1,21 @@
+//
+//  FamousSayingBakeCardview.swift
+//  Home
+//
+//  Created by Byeon jinha on 2023/09/04.
+//  Copyright © 2023 Wonji Suh. All rights reserved.
+//
+
+import SwiftUI
+
+struct FamousSayingBakeCardview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FamousSayingBakeCardview_Previews: PreviewProvider {
+    static var previews: some View {
+        FamousSayingBakeCardview()
+    }
+}
