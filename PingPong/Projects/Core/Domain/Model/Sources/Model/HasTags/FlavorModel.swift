1681562c453836449acdcc58ab949fe4c2e6a345
lainyzine <jinhaday@gmai.com>
2023-10-14 01:07:44 +0900

    🪛[chore] :  모듈 분리 작업

---
+++ PingPong/Projects/Core/Domain/Model/Sources/Model/HasTags/FlavorModel.swift
@@ -0,0 +1,38 @@
+//
+//  FavorModel.swift
+//  Model
+//
+//  Created by 서원지 on 2023/09/03.
+//  Copyright © 2023 Wonji Suh. All rights reserved.
+//
+
+import Foundation
+
+public enum Flavor: String {
+    case sweet = "달콤한 맛"
+    case salty = "짭짤한 맛"
+    case spicy = "매콤한 맛"
+    case nutty = "고소한 맛"
+    case light = "담백한 맛"
+    
+    public var type: FlavorType {
+        switch self {
+        case .light:
+            return FlavorType(english: "light", korean: "담백한 맛", ingredentImageName: .corn)
+        case .nutty:
+            return FlavorType(english: "nutty", korean: "고소한 맛", ingredentImageName: .cream)
+        case .salty:
+            return FlavorType(english: "salty", korean: "짭짤한 맛", ingredentImageName: .cheese)
+        case .spicy:
+            return FlavorType(english: "spicy", korean: "매콤한 맛", ingredentImageName: .jalapeno)
+        case .sweet:
+            return FlavorType(english: "sweet", korean: "달콤한 맛", ingredentImageName: .chocolate)
+        }
+    }
+    public struct FlavorType {
+        public let english: String
+        public let korean: String
+        public let ingredentImageName: Ingredent
+    }
+}
+
