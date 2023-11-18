9270f43a4e4827b735a6201664d58f53022e225c
Byeonjinha <jinhaday@gmail.com>
2023-11-18 21:26:55 +0900

    [feat] drop down

--- PingPong/Projects/Feature/Profile/Sources/UI/View/WithDraw/WithDrawView.swift
+++ PingPong/Projects/Feature/Profile/Sources/UI/View/WithDraw/WithDrawView.swift
@@ -29,7 +29,7 @@ struct WithDrawView: View {
             
             VStack {
                 topHeaderBackButton()
-                
+                CustomDropdownMenu()
                 Spacer()
             }
         }
@@ -67,3 +67,79 @@ struct WithDrawView: View {
 }
 
 
+struct CustomDropdownMenu: View {
+    @State var isSelecting = false
+    @State var selectionTitle = "Selected option"
+    @State var selectedRowId = 0
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text(selectionTitle)
+                    .font(.system(size: 16, weight: .semibold, design: .rounded))
+
+                Spacer()
+                Image(systemName: "chevron.down")
+                .font(.system(size: 16, weight: .semibold))
+            }
+            .padding(.horizontal)
+            .foregroundColor(.white)
+
+            if isSelecting {
+                Divider()
+                    .background(.white)
+                    .padding(.horizontal)
+
+                VStack(spacing: 5) {
+                    DropdownMenuItemView(isSelecting: $isSelecting, selectiontitle: $selectionTitle, selectionId: $selectedRowId, item: .init(id: 1, title: "Messages", iconImage: Image(systemName: "envelope"), onSelect: {}))
+                    DropdownMenuItemView(isSelecting: $isSelecting, selectiontitle: $selectionTitle, selectionId: $selectedRowId, item: .init(id: 2, title: "Archived", iconImage: Image(systemName: "archivebox"), onSelect: {}))
+                    DropdownMenuItemView(isSelecting: $isSelecting, selectiontitle: $selectionTitle, selectionId: $selectedRowId, item: .init(id: 3, title: "Trash", iconImage: Image(systemName: "trash"), onSelect: {}))
+                }
+
+            }
+
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical)
+        .background(Color(uiColor: UIColor.systemIndigo))
+        .cornerRadius(5)
+        .onTapGesture {
+            isSelecting.toggle()
+        }
+    }
+
+}
+
+struct DropdownMenuItemView: View {
+    @Binding var isSelecting: Bool
+    @Binding var selectiontitle: String
+    @Binding var selectionId: Int
+    
+    let item: DropdownItem
+
+    var body: some View {
+        Button(action: {
+            isSelecting = false
+            selectiontitle = item.title
+            item.onSelect()
+        }) {
+            HStack {
+                Image(systemName: "checkmark")
+                    .font(.system(size: 14, weight: .bold))
+                Text(item.title)
+                    .font(.system(size: 16, weight: .regular, design: .rounded))
+                Spacer()
+                item.iconImage
+            }
+            .padding(.horizontal)
+            .foregroundColor(.white)
+        }
+    }
+}
+
+struct DropdownItem: Identifiable {
+    let id: Int
+    let title: String
+    let iconImage: Image
+    let onSelect: () -> Void
+}
